e1c16d5d5f27705cd3f24cac9198f5371da548d3
Gabriel Alves Bizzi <gabriel.bizzi@lojastorra.com.br>
2025-10-29 12:46:44 -0300

    chore: initial commit

--- src/modules/ocr/services/ocr.service.ts
+++ src/modules/ocr/services/ocr.service.ts
@@ -23,27 +23,49 @@ export class OcrService {
 
     const { data } = await Tesseract.recognize(normalizedPath, 'por');
     const text = data.text.replace(/\s+/g, ' ');
+
     const hp = parseInt(text.match(/HP\s+(\d+)/)?.[1] ?? '0', 10);
     const mp = parseInt(text.match(/MP\s+(\d+)/)?.[1] ?? '0', 10);
     const attack = parseInt(text.match(/Ataque\s+(\d+)/)?.[1] ?? '0', 10);
     const defense = parseInt(text.match(/Defesa\s+(\d+)/)?.[1] ?? '0', 10);
-    const speedAttack = parseFloat(
-      text.match(/Vel(?:ocidade)? Ataque\s+([\d\.]+)/)?.[1] ?? '0',
+    const magicAttack = parseInt(
+      text.match(/Atq\s+M[aá]gico\s+(\d+)/)?.[1] ?? '0',
+      10,
     );
-    const critical = parseFloat(text.match(/Crí?tico\s+([\d\.]+)/)?.[1] ?? '0');
+    const speedAttack = parseFloat(
+      text.match(/Vel(?:ocidade)?\s+Ataque\s+([\d\.]+)/)?.[1] ?? '0',
+    );
+    const critical = parseFloat(
+      text.match(/Cr[ií]tico\s+([\d\.]+)/)?.[1] ?? '0',
+    );
     const criticalDamage = parseFloat(
-      text.match(/Dano Crí?tico\s+([\d\.]+)/)?.[1] ?? '0',
+      text.match(/Dano\s+Cr[ií]tico\s+([\d\.]+)/)?.[1] ?? '0',
+    );
+    const criticalDefense = parseFloat(
+      text.match(/Defesa\s+Cr[ií]tica\s+([\d\.]+)/)?.[1] ?? '0',
     );
     const pvpDamage = parseFloat(
-      text.match(/Dano PvP\s+([\d\.]+)/)?.[1] ?? '0',
+      text.match(/Dano\s+PvP\s+([\d\.]+)/)?.[1] ?? '0',
     );
     const pvpDefense = parseFloat(
-      text.match(/Defesa PvP\s+([\d\.]+)/)?.[1] ?? '0',
+      text.match(/Defesa\s+PvP\s+([\d\.]+)/)?.[1] ?? '0',
+    );
+    const penetration = parseFloat(
+      text.match(/Perfura[cç][aã]o\s+([\d\.]+)/)?.[1] ?? '0',
+    );
+    const absorption = parseFloat(
+      text.match(/Absor[cç][aã]o\s+([\d\.]+)/)?.[1] ?? '0',
     );
     const precision = parseFloat(
-      text.match(/Precisão\s+([\d\.]+)/)?.[1] ?? '0',
+      text.match(/Precis[aã]o\s+([\d\.]+)/)?.[1] ?? '0',
     );
-    const evasion = parseFloat(text.match(/Evasão\s+([\d\.]+)/)?.[1] ?? '0');
+    const evasion = parseFloat(text.match(/Evas[aã]o\s+([\d\.]+)/)?.[1] ?? '0');
+    const manaEconomy = parseFloat(
+      text.match(/Economia\s+de\s+Mana\s+([\d\.]+)/)?.[1] ?? '0',
+    );
+    const movement = parseFloat(
+      text.match(/Movimento\s+([\d\.]+)/)?.[1] ?? '0',
+    );
 
     await fs.unlink(normalizedPath);
 
@@ -52,13 +74,19 @@ export class OcrService {
       mp,
       attack,
       defense,
+      magicAttack,
       speedAttack,
       critical,
       criticalDamage,
+      criticalDefense,
       pvpDamage,
       pvpDefense,
+      penetration,
+      absorption,
       precision,
       evasion,
+      manaEconomy,
+      movement,
     };
   }
 }
